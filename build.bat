7abf1d0faec94b7fda64cd48e43bbe31d38c6f83
Yasser Arguelles <yasserarguelles2506@gmail.com>
2021-10-18 20:05:05 -0400

    Minor change to windows build file

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ call vcvars64
 
 if not exist "build/" mkdir build
 
-"G:\NASM\nasm.exe" src/lexer.asm -f win64 -o build/lexer.obj
+nasm src/lexer.asm -f win64 -o build/lexer.obj
 clang src/main.c -D_CRT_SECURE_NO_WARNINGS -g -gcodeview build/lexer.obj -o build/main.exe
